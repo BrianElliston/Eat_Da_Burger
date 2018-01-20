4cae7fd8ad33b88f0d80934a54e6e9735e248fa3
Brian Elliston <brianelliston@97-68-160-122.biz.bhn.net>
2018-01-20 13:37:23 -0500

    File structure

--- server.js
+++ server.js
@@ -4,3 +4,15 @@ var exp = require("express");
 var meth = require("method-override");
 var bodypars = require("body-parser");
 
+var app = express();
+
+var PORT = process.env.PORT || 8000;
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+
